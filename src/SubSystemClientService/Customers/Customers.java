ab003cb1c1949e9bdc0d89fa265413aba96f582a
Fedorov <chugun777akk@mail.ru>
2023-01-13 10:30:40 +0700

    add cls customers

--- src/SubSystemClientService/Customers/Customers.java
+++ src/SubSystemClientService/Customers/Customers.java
@@ -1,4 +1,43 @@
 package SubSystemClientService.Customers;
 
 public class Customers {
+    private int id;
+    private String name;
+    private String lastName;
+    private String patronymic;                    // отчество
+    private int numberPassport;
+    private int passportSeries;
+
+    public Customers(int id, String name, String lastName, String patronymic, int numberPassport, int passportSeries) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.patronymic = patronymic;
+        this.numberPassport = numberPassport;
+        this.passportSeries = passportSeries;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public int getNumberPassport() {
+        return numberPassport;
+    }
+
+    public int getPassportSeries() {
+        return passportSeries;
+    }
 }
