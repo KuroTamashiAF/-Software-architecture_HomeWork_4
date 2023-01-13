61e4108b3f86952d6671a5670db9d8ca8ced27f9
Fedorov <chugun777akk@mail.ru>
2023-01-13 14:51:34 +0700

    add method in cls Tiket

--- src/SubSystemClientService/Orders/Tiket.java
+++ src/SubSystemClientService/Orders/Tiket.java
@@ -1,4 +1,13 @@
 package SubSystemClientService.Orders;
 
 public class Tiket {
+    private int idTiket;
+
+    public Tiket() {
+        this.idTiket = (int)Math.random()*100;
+    }
+
+    public int getIdTiket() {
+        return idTiket;
+    }
 }
